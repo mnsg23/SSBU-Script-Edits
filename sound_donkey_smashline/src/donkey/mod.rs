b6874b20c4c0018ae77da36c67a6bab336836388
mnsg23 <73201803+mnsg23@users.noreply.github.com>
2021-09-09 08:30:40 -0400

    Initial commit

---
+++ sound_donkey_smashline/src/donkey/mod.rs
@@ -0,0 +1,53 @@
+use smash::phx::Hash40;
+use smash::lua2cpp::L2CAgentBase;
+use smashline::*;
+use smash_script::*;
+
+#[acmd_script(agent = "donkey", script = "sound_batswing4", category = ACMD_SOUND)]
+unsafe fn donkey_sound_batswing4(fighter: &mut L2CAgentBase) {
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 45.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::PLAY_SE(fighter, Hash40::new("vc_donkey_attack07"));
+	}
+}
+
+#[acmd_script(agent = "donkey", script = "sound_itemheavyget", category = ACMD_SOUND)]
+unsafe fn donkey_sound_itemheavyget(fighter: &mut L2CAgentBase) {
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 6.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::PLAY_SE(fighter, Hash40::new("vc_donkey_heavyget"));
+	}
+}
+
+#[acmd_script(agent = "donkey", script = "sound_lipstickswing4", category = ACMD_SOUND)]
+unsafe fn donkey_sound_lipstickswing4(fighter: &mut L2CAgentBase) {
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 8.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::STOP_SE(fighter, Hash40::new("se_common_smash_start"));
+	}
+	smash::app::sv_animcmd::wait(fighter.lua_state_agent, 3.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::PLAY_SE(fighter, Hash40::new("vc_donkey_attack05"));
+	}
+}
+
+#[acmd_script(agent = "donkey", script = "sound_starrodswing4", category = ACMD_SOUND)]
+unsafe fn donkey_sound_starrodswing4(fighter: &mut L2CAgentBase) {
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 8.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::STOP_SE(fighter, Hash40::new("se_common_smash_start"));
+	}
+	smash::app::sv_animcmd::wait(fighter.lua_state_agent, 3.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::PLAY_SE(fighter, Hash40::new("vc_donkey_attack05"));
+	}
+}
+
+pub fn install() {
+	smashline::install_acmd_scripts!(
+		donkey_sound_batswing4,
+		donkey_sound_itemheavyget,
+		donkey_sound_lipstickswing4,
+		donkey_sound_starrodswing4,
+	);
+}
