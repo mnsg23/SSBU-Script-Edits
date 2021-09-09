d5ed5706f52cfff6fb9dd369c21998f7683cd928
mnsg23 <73201803+mnsg23@users.noreply.github.com>
2021-09-09 08:37:22 -0400

    Initial commit

---
+++ sound_rosetta_smashline/src/rosetta/mod.rs
@@ -0,0 +1,65 @@
+use smash::phx::Hash40;
+use smash::lua2cpp::L2CAgentBase;
+use smashline::*;
+use smash_script::*;
+
+#[acmd_script(agent = "rosetta", script = "sound_batswing4", category = ACMD_SOUND)]
+unsafe fn rosetta_sound_batswing4(fighter: &mut L2CAgentBase) {
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 45.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::PLAY_SE(fighter, Hash40::new("vc_rosetta_attack05"));
+	}
+}
+
+#[acmd_script(agent = "rosetta", script = "sound_clubswing4", category = ACMD_SOUND)]
+unsafe fn rosetta_sound_clubswing4(fighter: &mut L2CAgentBase) {
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 8.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::STOP_SE(fighter, Hash40::new("se_common_smash_start"));
+		smash_script::macros::PLAY_SE(fighter, Hash40::new("vc_rosetta_attack05"));
+	}
+}
+
+#[acmd_script(agent = "rosetta", script = "sound_lipstickswing4", category = ACMD_SOUND)]
+unsafe fn rosetta_sound_lipstickswing4(fighter: &mut L2CAgentBase) {
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 8.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::STOP_SE(fighter, Hash40::new("se_common_smash_start"));
+		smash_script::macros::PLAY_SE(fighter, Hash40::new("vc_rosetta_attack05"));
+	}
+}
+
+#[acmd_script(agent = "rosetta", script = "sound_starrodswing4", category = ACMD_SOUND)]
+unsafe fn rosetta_sound_starrodswing4(fighter: &mut L2CAgentBase) {
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 8.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::STOP_SE(fighter, Hash40::new("se_common_smash_start"));
+		smash_script::macros::PLAY_SE(fighter, Hash40::new("vc_rosetta_attack05"));
+	}
+}
+
+#[acmd_script(agent = "rosetta", script = "sound_win3", category = ACMD_SOUND)]
+unsafe fn rosetta_sound_win3(fighter: &mut L2CAgentBase) {
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 5.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::PLAY_SE_NO_3D(fighter, Hash40::new("vc_rosetta_win03"));
+	}
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 20.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::PLAY_SE(fighter, Hash40::new("se_rosetta_swing_m_win03"));
+	}
+	smash::app::sv_animcmd::frame(fighter.lua_state_agent, 107.0);
+	if macros::is_excute(fighter) {
+		smash_script::macros::PLAY_SE(fighter, Hash40::new("se_tico_return_win03"));
+	}
+}
+
+pub fn install() {
+	smashline::install_acmd_scripts!(
+		rosetta_sound_batswing4,
+		rosetta_sound_clubswing4,
+		rosetta_sound_lipstickswing4,
+		rosetta_sound_starrodswing4,
+		rosetta_sound_win3,
+	);
+}
