7b4b2dea85bc2a4c2e5bd25b8bccce3dccdb2ffc
mnsg23 <73201803+mnsg23@users.noreply.github.com>
2021-09-30 22:03:48 -0400

    Script cleaning
    Reduced the amount of duplicate scripts.

--- sound_donkey_smashline/src/donkey/mod.rs
+++ sound_donkey_smashline/src/donkey/mod.rs
@@ -1,8 +1,8 @@
 use smash::phx::*;
 use smash::lua2cpp::L2CAgentBase;
 use smash::app::sv_animcmd::*;
-use smashline::*;
 use smash_script::*;
+use smashline::*;
 
 #[acmd_script(agent = "donkey", script = "sound_batswing4", category = ACMD_SOUND)]
 unsafe fn donkey_sound_batswing4(fighter: &mut L2CAgentBase) {
@@ -20,20 +20,8 @@ unsafe fn donkey_sound_itemheavyget(fighter: &mut L2CAgentBase) {
 	}
 }
 
-#[acmd_script(agent = "donkey", script = "sound_lipstickswing4", category = ACMD_SOUND)]
-unsafe fn donkey_sound_lipstickswing4(fighter: &mut L2CAgentBase) {
-	frame(fighter.lua_state_agent, 8.0);
-	if macros::is_excute(fighter) {
-		macros::STOP_SE(fighter, Hash40::new("se_common_smash_start"));
-	}
-	wait(fighter.lua_state_agent, 3.0);
-	if macros::is_excute(fighter) {
-		macros::PLAY_SE(fighter, Hash40::new("vc_donkey_attack05"));
-	}
-}
-
-#[acmd_script(agent = "donkey", script = "sound_starrodswing4", category = ACMD_SOUND)]
-unsafe fn donkey_sound_starrodswing4(fighter: &mut L2CAgentBase) {
+#[acmd_script(agent = "donkey", scripts = ["sound_lipstickswing4", "sound_starrodswing4"], category = ACMD_SOUND)]
+unsafe fn donkey_sound_itemswing4(fighter: &mut L2CAgentBase) {
 	frame(fighter.lua_state_agent, 8.0);
 	if macros::is_excute(fighter) {
 		macros::STOP_SE(fighter, Hash40::new("se_common_smash_start"));
@@ -48,7 +36,6 @@ pub fn install() {
 	smashline::install_acmd_scripts!(
 		donkey_sound_batswing4,
 		donkey_sound_itemheavyget,
-		donkey_sound_lipstickswing4,
-		donkey_sound_starrodswing4,
+		donkey_sound_itemswing4,
 	);
 }
