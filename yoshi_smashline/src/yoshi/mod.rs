25cf91fe99e42c735aa79d39e4804228206f7270
mnsg23 <73201803+mnsg23@users.noreply.github.com>
2022-01-01 18:13:49 -0500

    Script update

--- yoshi_smashline/src/yoshi/mod.rs
+++ yoshi_smashline/src/yoshi/mod.rs
@@ -645,8 +645,7 @@ unsafe fn yoshi_game_specialairlw(fighter: &mut L2CAgentBase) {
 	}
 	frame(fighter.lua_state_agent, 20.0);
 	if macros::is_excute(fighter) {
-		macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 16.0, 80, 76, 0, 60, 7.0, 0.0, 5.5, 0.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 10, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_KICK, *ATTACK_REGION_HIP);
-		AttackModule::set_attack_height_all(fighter.module_accessor, AttackHeight(*ATTACK_HEIGHT_HIGH), false);
+		attack!(fighter, *MA_MSC_CMD_ATTACK_NODE, 0, Hash40::new("top"), 0.0, 5.5, 0.0);
 	}
 }
 
