05aca5ce534d321b2c99c87ecdb1a754a218436f
mnsg23 <73201803+mnsg23@users.noreply.github.com>
2022-02-03 14:15:23 -0500

    Initial commit

---
+++ mewtwo_smashline/src/lib.rs
@@ -0,0 +1,16 @@
+#![feature(concat_idents)]
+#![feature(proc_macro_hygiene)]
+#![feature(asm)]
+
+mod mewtwo;
+pub static mut FIGHTER_CUTIN_MANAGER_ADDR: usize = 0;
+
+macro_rules! c_str {($l:tt) => {[$l.as_bytes(), "\u{0}".as_bytes()].concat().as_ptr();};}
+
+#[skyline::main(name = "mewtwo_smashline")]
+pub fn main() {
+	mewtwo::install();
+	unsafe {
+		skyline::nn::ro::LookupSymbol(&mut FIGHTER_CUTIN_MANAGER_ADDR, c_str!("_ZN3lib9SingletonIN3app19FighterCutInManagerEE9instance_E"));
+	}
+}
