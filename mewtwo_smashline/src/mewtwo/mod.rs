2168afa6587e275feefe08e7fceb0426fc638543
mnsg23 <73201803+mnsg23@users.noreply.github.com>
2022-07-05 09:47:21 -0400

    Script updates
    Affected scripts include "game_attackairf" and "game_attackairlw".

--- mewtwo_smashline/src/mewtwo/mod.rs
+++ mewtwo_smashline/src/mewtwo/mod.rs
@@ -145,8 +145,8 @@ unsafe fn mewtwo_game_attackairf(fighter: &mut L2CAgentBase) {
 	macros::FT_MOTION_RATE(fighter, 1.0);
 	frame(fighter.lua_state_agent, 6.0);
 	if macros::is_excute(fighter) {
-		macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 13.0, 45, 103, 0, 40, 5.5, 0.0, 8.2, 13.0, None, None, None, 1.3, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 3, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_purple"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_PUNCH);
-		macros::ATTACK(fighter, 1, 0, Hash40::new("top"), 13.0, 45, 103, 0, 40, 4.5, 0.0, 8.2, 7.0, None, None, None, 1.3, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 3, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_purple"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_PUNCH);
+		macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 13.0, 45, 103, 0, 40, 6.0, 0.0, 8.2, 13.0, None, None, None, 1.3, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 3, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_purple"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_PUNCH);
+		macros::ATTACK(fighter, 1, 0, Hash40::new("top"), 13.0, 45, 103, 0, 40, 5.0, 0.0, 8.2, 7.0, None, None, None, 1.3, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 3, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_purple"), *ATTACK_SOUND_LEVEL_L, *COLLISION_SOUND_ATTR_CUTUP, *ATTACK_REGION_PUNCH);
 	}
 	frame(fighter.lua_state_agent, 9.0);
 	if macros::is_excute(fighter) {
@@ -183,7 +183,7 @@ unsafe fn mewtwo_game_attackairhi(fighter: &mut L2CAgentBase) {
 #[acmd_script(agent = "mewtwo", script = "game_attackairlw", category = ACMD_GAME)]
 unsafe fn mewtwo_game_attackairlw(fighter: &mut L2CAgentBase) {
 	frame(fighter.lua_state_agent, 1.0);
-	macros::FT_MOTION_RATE(fighter, 0.75);
+	macros::FT_MOTION_RATE(fighter, 0.6875);
 	frame(fighter.lua_state_agent, 4.0);
 	if macros::is_excute(fighter) {
 		WorkModule::on_flag(fighter.module_accessor, *FIGHTER_STATUS_ATTACK_AIR_FLAG_ENABLE_LANDING);
