a5a39b2fb77b9dfd9bec35b8240c794b00a00b8f
mnsg23 <73201803+mnsg23@users.noreply.github.com>
2023-03-21 12:55:42 -0400

    Script updates

--- sound_donkey_smashline/src/donkey/mod.rs
+++ sound_donkey_smashline/src/donkey/mod.rs
@@ -1,6 +1,6 @@
 use {
 	smash::{
-		app::sv_animcmd::*,
+		app::sv_animcmd,
 		lua2cpp::L2CAgentBase,
 		phx::Hash40
 	},
@@ -10,7 +10,7 @@ use {
 
 #[acmd_script(agent = "donkey", script = "sound_batswing4", category = ACMD_SOUND)]
 unsafe fn donkey_sound_batswing4(fighter: &mut L2CAgentBase) {
-	frame(fighter.lua_state_agent, 45.0);
+	sv_animcmd::frame(fighter.lua_state_agent, 45.0);
 	if macros::is_excute(fighter) {
 		macros::PLAY_SE(fighter, Hash40::new("vc_donkey_attack07"));
 	}
@@ -18,7 +18,7 @@ unsafe fn donkey_sound_batswing4(fighter: &mut L2CAgentBase) {
 
 #[acmd_script(agent = "donkey", script = "sound_itemheavyget", category = ACMD_SOUND)]
 unsafe fn donkey_sound_itemheavyget(fighter: &mut L2CAgentBase) {
-	frame(fighter.lua_state_agent, 6.0);
+	sv_animcmd::frame(fighter.lua_state_agent, 6.0);
 	if macros::is_excute(fighter) {
 		macros::PLAY_SE(fighter, Hash40::new("vc_donkey_heavyget"));
 	}
@@ -26,11 +26,11 @@ unsafe fn donkey_sound_itemheavyget(fighter: &mut L2CAgentBase) {
 
 #[acmd_script(agent = "donkey", scripts = ["sound_lipstickswing4", "sound_starrodswing4"], category = ACMD_SOUND)]
 unsafe fn donkey_sound_itemswing4(fighter: &mut L2CAgentBase) {
-	frame(fighter.lua_state_agent, 8.0);
+	sv_animcmd::frame(fighter.lua_state_agent, 8.0);
 	if macros::is_excute(fighter) {
 		macros::STOP_SE(fighter, Hash40::new("se_common_smash_start"));
 	}
-	wait(fighter.lua_state_agent, 3.0);
+	sv_animcmd::wait(fighter.lua_state_agent, 3.0);
 	if macros::is_excute(fighter) {
 		macros::PLAY_SE(fighter, Hash40::new("vc_donkey_attack05"));
 	}
