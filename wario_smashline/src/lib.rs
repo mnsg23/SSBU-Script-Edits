c08fff291b5db9cce184984976f5762f99a80c4d
mnsg23 <73201803+mnsg23@users.noreply.github.com>
2023-03-21 19:29:09 -0400

    Update

--- wario_smashline/src/lib.rs
+++ wario_smashline/src/lib.rs
@@ -1,16 +1,15 @@
-#![feature(concat_idents)]
-#![feature(proc_macro_hygiene)]
-#![feature(asm)]
+#![feature(
+	concat_idents,
+	proc_macro_hygiene
+)]
+#![allow(
+	unused_macros,
+	clippy::borrow_interior_mutable_const
+)]
 
 mod wario;
-pub static mut FIGHTER_CUTIN_MANAGER_ADDR: usize = 0;
-
-macro_rules! c_str {($l:tt) => {[$l.as_bytes(), "\u{0}".as_bytes()].concat().as_ptr();};}
 
 #[skyline::main(name = "wario_smashline")]
 pub fn main() {
 	wario::install();
-	unsafe {
-		skyline::nn::ro::LookupSymbol(&mut FIGHTER_CUTIN_MANAGER_ADDR, c_str!("_ZN3lib9SingletonIN3app19FighterCutInManagerEE9instance_E"));
-	}
 }
