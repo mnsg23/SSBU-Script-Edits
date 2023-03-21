f88efef9683fa9a49753d1c8ab74b7cb93b7c41f
mnsg23 <73201803+mnsg23@users.noreply.github.com>
2023-03-21 13:14:33 -0400

    Script updates

--- sound_rosetta_smashline/src/rosetta/mod.rs
+++ sound_rosetta_smashline/src/rosetta/mod.rs
@@ -1,6 +1,6 @@
 use {
 	smash::{
-		app::sv_animcmd::*,
+		app::sv_animcmd,
 		lua2cpp::L2CAgentBase,
 		phx::Hash40
 	},
@@ -10,7 +10,7 @@ use {
 
 #[acmd_script(agent = "rosetta", script = "sound_batswing4", category = ACMD_SOUND)]
 unsafe fn rosetta_sound_batswing4(fighter: &mut L2CAgentBase) {
-	frame(fighter.lua_state_agent, 45.0);
+	sv_animcmd::frame(fighter.lua_state_agent, 45.0);
 	if macros::is_excute(fighter) {
 		macros::PLAY_SE(fighter, Hash40::new("vc_rosetta_attack05"));
 	}
@@ -18,7 +18,7 @@ unsafe fn rosetta_sound_batswing4(fighter: &mut L2CAgentBase) {
 
 #[acmd_script(agent = "rosetta", scripts = ["sound_clubswing4", "sound_lipstickswing4", "sound_starrodswing4"], category = ACMD_SOUND)]
 unsafe fn rosetta_sound_itemswing4(fighter: &mut L2CAgentBase) {
-	frame(fighter.lua_state_agent, 8.0);
+	sv_animcmd::frame(fighter.lua_state_agent, 8.0);
 	if macros::is_excute(fighter) {
 		macros::STOP_SE(fighter, Hash40::new("se_common_smash_start"));
 		macros::PLAY_SE(fighter, Hash40::new("vc_rosetta_attack05"));
@@ -27,15 +27,15 @@ unsafe fn rosetta_sound_itemswing4(fighter: &mut L2CAgentBase) {
 
 #[acmd_script(agent = "rosetta", script = "sound_win3", category = ACMD_SOUND)]
 unsafe fn rosetta_sound_win3(fighter: &mut L2CAgentBase) {
-	frame(fighter.lua_state_agent, 5.0);
+	sv_animcmd::frame(fighter.lua_state_agent, 5.0);
 	if macros::is_excute(fighter) {
 		macros::PLAY_SE_NO_3D(fighter, Hash40::new("vc_rosetta_win03"));
 	}
-	frame(fighter.lua_state_agent, 20.0);
+	sv_animcmd::frame(fighter.lua_state_agent, 20.0);
 	if macros::is_excute(fighter) {
 		macros::PLAY_SE(fighter, Hash40::new("se_rosetta_swing_m_win03"));
 	}
-	frame(fighter.lua_state_agent, 107.0);
+	sv_animcmd::frame(fighter.lua_state_agent, 107.0);
 	if macros::is_excute(fighter) {
 		macros::PLAY_SE(fighter, Hash40::new("se_tico_return_win03"));
 	}
