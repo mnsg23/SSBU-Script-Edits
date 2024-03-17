a587a56f99b2fce28532e8f5159d6ee6237f1bbe
mnsg23 <73201803+mnsg23@users.noreply.github.com>
2024-03-17 17:28:46 -0400

    Smashline 2 Migration

--- sound_donkey_smashline/src/donkey/mod.rs
+++ sound_donkey_smashline/src/donkey/mod.rs
@@ -8,24 +8,21 @@ use {
 	smashline::*
 };
 
-#[acmd_script(agent = "donkey", script = "sound_batswing4", category = ACMD_SOUND)]
-unsafe fn donkey_sound_batswing4(fighter: &mut L2CAgentBase) {
+unsafe extern "C" fn donkey_sound_batswing4(fighter: &mut L2CAgentBase) {
 	sv_animcmd::frame(fighter.lua_state_agent, 45.0);
 	if macros::is_excute(fighter) {
 		macros::PLAY_SE(fighter, Hash40::new("vc_donkey_attack07"));
 	}
 }
 
-#[acmd_script(agent = "donkey", script = "sound_itemheavyget", category = ACMD_SOUND)]
-unsafe fn donkey_sound_itemheavyget(fighter: &mut L2CAgentBase) {
+unsafe extern "C" fn donkey_sound_itemheavyget(fighter: &mut L2CAgentBase) {
 	sv_animcmd::frame(fighter.lua_state_agent, 6.0);
 	if macros::is_excute(fighter) {
 		macros::PLAY_SE(fighter, Hash40::new("vc_donkey_heavyget"));
 	}
 }
 
-#[acmd_script(agent = "donkey", scripts = ["sound_lipstickswing4", "sound_starrodswing4"], category = ACMD_SOUND)]
-unsafe fn donkey_sound_itemswing4(fighter: &mut L2CAgentBase) {
+unsafe extern "C" fn donkey_sound_itemswing4(fighter: &mut L2CAgentBase) {
 	sv_animcmd::frame(fighter.lua_state_agent, 8.0);
 	if macros::is_excute(fighter) {
 		macros::STOP_SE(fighter, Hash40::new("se_common_smash_start"));
@@ -37,9 +34,10 @@ unsafe fn donkey_sound_itemswing4(fighter: &mut L2CAgentBase) {
 }
 
 pub fn install() {
-	smashline::install_acmd_scripts!(
-		donkey_sound_batswing4,
-		donkey_sound_itemheavyget,
-		donkey_sound_itemswing4,
-	);
+	Agent::new("donkey")
+		.sound_acmd("sound_batswing4", donkey_sound_batswing4)
+		.sound_acmd("sound_itemheavyget", donkey_sound_itemheavyget)
+		.sound_acmd("sound_lipstickswing4", donkey_sound_itemswing4)
+		.sound_acmd("sound_starrodswing4", donkey_sound_itemswing4)
+		.install();
 }
